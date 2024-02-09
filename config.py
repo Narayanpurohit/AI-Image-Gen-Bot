811d07c40bdbb9276acce5c4d7dbf179c97eab5a
Narayanpurohit <88087099+Narayanpurohit@users.noreply.github.com>
2024-02-09 18:05:01 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-api_id = int("your api id")
-api_hash = "your api hash"
-bot_token = "your bot token"
+api_id = int("15191874")
+api_hash = "3037d39233c6fad9b80d83bb8a339a07"
+bot_token = "6921940099:AAGtYj2uBfnVtP1arsXMdZP_Jf0DWblAREk"
 
 MAX_GEN = 10
-MONGOURI = "Your MONGO DB URI"
-OWNER_ID = int("owner telegram id")
+MONGOURI = "mongodb+srv://1by1themes:IOIfm2sf0ubXeSPu@cluster0.gorxek5.mongodb.net/?retryWrites=true&w=majority"
+OWNER_ID = int("6789146594")
